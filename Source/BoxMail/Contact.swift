bcfed0f404233a1fb08c73d64bdd06b66c486d32
Raj Abishek <rajabishek@hotmail.com>
2016-08-08 23:27:40 +0530

    Adds the contact model

---
+++ Source/BoxMail/Contact.swift
@@ -0,0 +1,18 @@
+//
+//  Contact.swift
+//  BoxMail
+//
+//  Created by Raj Abishek on 08/08/16.
+//  Copyright © 2016 Raj Abishek. All rights reserved.
+//
+
+import UIKit
+
+struct Contact {
+    
+    var name: String
+    
+    var emailAddress: String
+    
+    var profileImageName: Int
+}
---
+++ Source/BoxMail/Email.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  BoxMail
+//
+//  Created by Raj Abishek on 08/08/16.
+//  Copyright © 2016 Raj Abishek. All rights reserved.
+//
+
+import Foundation
