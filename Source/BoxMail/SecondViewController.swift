a2e802a2e8be3d61773133e03dc4c2a2572bbafc
Raj Abishek <rajabishek@hotmail.com>
2016-08-09 00:01:19 +0530

    Removes the inti for the second view controller

--- Source/BoxMail/SecondViewController.swift
+++ Source/BoxMail/SecondViewController.swift
@@ -11,19 +11,10 @@ import XLPagerTabStrip
 
 class SecondViewController: UIViewController, IndicatorInfoProvider {
     
-    var itemInfo = IndicatorInfo(title: "Second")
+    var itemInfo = IndicatorInfo(title: "Social")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    init(itemInfo: IndicatorInfo) {
-        self.itemInfo = itemInfo
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     func indicatorInfoForPagerTabStrip(pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
