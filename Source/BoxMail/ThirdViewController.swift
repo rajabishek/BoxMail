16549ecaf67e98723ae0e23c44b3a730a65c0eb3
Raj Abishek <rajabishek@hotmail.com>
2016-08-09 00:02:00 +0530

    Removes the visual frame drwing for the 3rd view controller

--- Source/BoxMail/ThirdViewController.swift
+++ Source/BoxMail/ThirdViewController.swift
@@ -11,19 +11,10 @@ import XLPagerTabStrip
 
 class ThirdViewController: UIViewController, IndicatorInfoProvider {
     
-    var itemInfo = IndicatorInfo(title: "Third")
+    var itemInfo = IndicatorInfo(title: "Promotional")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    init(itemInfo: IndicatorInfo) {
-        self.itemInfo = itemInfo
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     func indicatorInfoForPagerTabStrip(pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
