310ba3e55f4c7aeb2b819b00a2ca6d2736a907f5
Raj Abishek <rajabishek@hotmail.com>
2016-08-08 23:02:45 +0530

    Adds the initial project single view app + core data stack from xcode

---
+++ Source/BoxMail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoxMail
+//
+//  Created by Raj Abishek on 08/08/16.
+//  Copyright © 2016 Raj Abishek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
